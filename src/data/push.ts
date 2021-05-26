864ed57301613f6a49ef1ff426213a71d62ea88d
davidevaliante <dav.valiante@gmail.com>
2021-05-26 12:09:50 +0200

    updated api endpoint for production

--- src/data/push.ts
+++ src/data/push.ts
@@ -2,9 +2,9 @@ import axios from 'axios'
 import { Spin } from '../models/Spin'
 import { Symbol } from '../models/Symbol'
 
-// const API_ROOT = 'https://monopoly.spike-realtime-api.eu/api'
+const API_ROOT = 'https://monopoly.spike-realtime-api.eu/api'
 
-const API_ROOT = 'http://localhost:5002/api'
+// const API_ROOT = 'http://localhost:5002/api'
 
 
 export const pushRows = async (rows : Spin[]) => {
